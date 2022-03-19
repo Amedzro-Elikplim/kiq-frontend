5078939862d26029c1f2e5f5c6c07dcee0acd76a
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-03-19 16:01:06 +0000

    Display message from server on client side

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const form = document.getElementById('btn');
+const li = document.createElement('li');
+const chatList = document.querySelector('.chat');
 
 // eslint-disable-next-line no-undef
 const socket = io('http://localhost:3000', {
@@ -9,9 +11,17 @@ form.addEventListener('click', (e) => {
   const message = document.getElementById('message');
   e.preventDefault();
   const { value } = message;
+  li.classList.toggle('aliceblue');
 
   if (value) {
     socket.emit('message', value);
   }
   message.value = '';
+});
+
+socket.on('chat message', (msg) => {
+  li.textContent = msg;
+  chatList.appendChild(li);
+  li.classList.toggle('aliceblue');
+  window.scrollTo(0, document.body.scrollHeight);
 });
